3c1866147d5069d91e203a4842c3dbacb022fc04
James Nicholson <git@nicholson.io>
2023-05-11 23:14:34 +0200

    Fix imports, nuke unused initializer, clean up

--- src/ForestNo.sol
+++ src/ForestNo.sol
@@ -1,54 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/security/Pausable.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/utils/Counters.sol";
 
-contract ForestNo is Initializable, ERC721Upgradeable, PausableUpgradeable, OwnableUpgradeable {
+contract ForestNo is ERC721, Pausable, Ownable {
 
     event Minted(address indexed to, uint256 indexed tokenId);
 
     uint256 private constant MAX_NFTS_PER_ADDRESS = 5;
+    uint256 private constant TOKEN_MAX_CAP = 328;
+
     bool private _presale = true;
     bool private _contractLive = false;
-    uint256 private TOKEN_MAX_CAP = 328;
     uint256 private _tokenCount = 0;
     uint256 private _mintPrice = 0;
-    string public URI;
+    string public _uri = "https://www.mymetadata.com/token/";
 
     mapping(address => bool) private _whitelist;
 
-    using CountersUpgradeable for CountersUpgradeable.Counter;
-    mapping(address => CountersUpgradeable.Counter) private _mintedNFTs;
+    using Counters for Counters.Counter;
+    mapping(address => Counters.Counter) private _mintedNFTs;
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() 
-    {
-        _disableInitializers();
-        
-        _transferOwnership(msg.sender);
-        URI = "https://www.mymetadata.com/token/";
-    }
-
-    function initialize() initializer public 
-    {
-        __ERC721_init("Forest, No", "FRSTNO");
-        __Pausable_init();
-        __Ownable_init();
-
-    }
+    constructor() ERC721("Forest, No", "FRSTNO") {}
 
     function setMintPrice(uint256 newPrice) public onlyOwner
     {
         _mintPrice = newPrice;
     }
 
-    function setURI (string calldata _uri) external onlyOwner 
+    function setURI (string calldata uri) external onlyOwner 
     {
-        URI = _uri;
+        _uri = uri;
     }
 
     function togglePresale() public onlyOwner 
@@ -71,6 +56,7 @@ contract ForestNo is Initializable, ERC721Upgradeable, PausableUpgradeable, Owna
     {
         _unpause();
     }
+
     function addToWhitelist(address account) public onlyOwner {
         _whitelist[account] = true;
     }
@@ -91,7 +77,6 @@ contract ForestNo is Initializable, ERC721Upgradeable, PausableUpgradeable, Owna
         }
     }
 
-
     function removeFromWhitelist(address account) public onlyOwner {
         _whitelist[account] = false;
     }
@@ -105,12 +90,12 @@ contract ForestNo is Initializable, ERC721Upgradeable, PausableUpgradeable, Owna
     {
         require(_contractLive, "Contract is not live yet, please try again later!");
         require(msg.value >= _mintPrice, "You need to pay more than the mint price to be able to mint!");
-        require(_mintedNFTs[_msgSender()].current() < MAX_NFTS_PER_ADDRESS, "MyToken: Max NFTs per address reached");
+        require(_mintedNFTs[_msgSender()].current() < MAX_NFTS_PER_ADDRESS, "Max NFTs per address reached");
         require(_tokenCount < TOKEN_MAX_CAP);
 
         if (_presale) {
-            require(_whitelist[msg.sender], "MyToken: Address not whitelisted during presale");
-            require(_mintedNFTs[_msgSender()].current() < 1, "MyToken: Max NFTs per address reached");
+            require(_whitelist[msg.sender], "Address not whitelisted during presale");
+            require(_mintedNFTs[_msgSender()].current() < 1, "Max NFTs per address reached");
         }
         
         _tokenCount = _tokenCount + 1;
@@ -119,29 +104,12 @@ contract ForestNo is Initializable, ERC721Upgradeable, PausableUpgradeable, Owna
         emit Minted(_msgSender(), _tokenCount);
     }
 
-    function safeMint(address to) public onlyOwner 
-    {
-        require(_contractLive, "Contract is not live yet, please try again later!");
-        require(_mintedNFTs[to].current() < MAX_NFTS_PER_ADDRESS, "MyToken: Max NFTs per address reached");
-        require(_tokenCount < TOKEN_MAX_CAP);
-        // if (_presale) {
-        //     require(_whitelist[msg.sender], "MyToken: Address not whitelisted during presale");
-        //     require(_mintedNFTs[to].current() < 1, "MyToken: Max NFTs per address reached");
-        // }
-        _tokenCount = _tokenCount + 1;
-        _safeMint(to, _tokenCount);
-        _mintedNFTs[to].increment();
-        emit Minted(to, _tokenCount);
-    }
-
-
     function withdraw() public onlyOwner 
     {
         uint256 balance = address(this).balance;
-        require(balance > 0, "MyToken: No funds available for withdrawal");
+        require(balance > 0, "No funds available for withdrawal");
         payable(msg.sender).transfer(balance);
     }
-
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
@@ -154,18 +122,8 @@ contract ForestNo is Initializable, ERC721Upgradeable, PausableUpgradeable, Owna
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) 
     {
         require(_exists(tokenId), "MyToken: URI query for nonexistent token");
-        return string(abi.encodePacked(URI, _toString(tokenId), ".json"));
+        return string(abi.encodePacked(_uri, _toString(tokenId), ".json"));
     }
-
-// function tokenURI(uint tokenId) public view override(ERC721Upgradeable) returns (string memory) {
-//     require(tokenId > 0 && tokenId <= config.supply, "15");
-//     if (bytes(config.base).length > 0) {
-//       return string(abi.encodePacked(config.base, _toString(tokenId), ".json"));
-//     } else {
-//       return bytes(config.placeholder).length > 0 ?  config.placeholder : "ipfs://bafkreieqcdphcfojcd2vslsxrhzrjqr6cxjlyuekpghzehfexi5c3w55eq";
-//     }
-//   }
-
 
     function _toString(uint value) internal pure returns (string memory) 
     {
--- test/ForestNo.t.sol
+++ test/ForestNo.t.sol
@@ -2,15 +2,45 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "src/ForestNo.sol";
 
 contract ForestNoTest is Test {
     ForestNo public forestNo;
+    address constant tester = address(1);
 
     function setUp() public {
         forestNo = new ForestNo();
+        forestNo.transferOwnership(tester);
     }
 
     function testMint() public {
+        // Give test user some ether
+        vm.deal(tester, 10 ether);
+        assertEq(forestNo.balanceOf(tester), 0);
+
+        // Become test user
+        vm.startPrank(tester);
+
+        // Mint should fail (not live yet)
+        vm.expectRevert("Contract is not live yet, please try again later!");
+        forestNo.mint();
+
+        // Make contract live
+        forestNo.setLiveState();
+
+        // Mint should fail (not whitelisted)
+        vm.expectRevert("Address not whitelisted during presale");
+        forestNo.mint();
+
+        // Whitelist user
+        forestNo.addToWhitelist(tester);
+
+        // Mint should succeed
+        forestNo.mint();
+        assertEq(forestNo.balanceOf(tester), 1);
+
+        // Next mint should fail (max per address during presale)
+        vm.expectRevert("Max NFTs per address reached");
         forestNo.mint();
     }
 }
